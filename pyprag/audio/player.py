62915136b5d1b7095dfd95efd706c8f0944c1262
Sebastien Le Maguer <lemagues@tcd.ie>
2020-03-12 07:54:49 +0000

    [audio] clean and document package

--- pyprag/audio/player.py
+++ pyprag/audio/player.py
@@ -1,12 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+AUTHOR
+
+    Sébastien Le Maguer <lemagues@tcd.ie>
+
+DESCRIPTION
+
+    Module containing the player class and the player object which should be used. This object is accessible via "pygrad.audio.player.player"
+
+LICENSE
+"""
 
 import sounddevice as sd
 
+###############################################################################
+# Classes
+###############################################################################
+
 class Player:
-    def __init__(self):
-        pass
+    """Class encapsulating an audio player.
 
-    def play(self, array, sr, viewBox=None):
-         sd.play(array, sr)
-         status = sd.wait()
+    """
 
+    def play(self, sig_array, sr, viewBox=None):
+        """Play the signal given in parameters
+
+        Parameters
+        ----------
+        sig_array : np.array
+           The signal samples
+
+        sr : int
+           The sample rate
+
+        viewBox: pg.pyqtgraph.ViewBox
+           The view box currently active
+        """
+        sd.play(sig_array, sr)
+        status = sd.wait()
+
+
+###############################################################################
+# Shared variables
+###############################################################################
+
+# The actual player
 player = Player()
