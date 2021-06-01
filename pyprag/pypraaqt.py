6493fa15fe35bdb1c1647771ccf4c305eab6090c
Sebastien Le Maguer <lemagues@tcd.ie>
2021-06-01 21:41:35 +0100

    [scripts] update: make pypraaqt ready to be an entry point script

--- pyprag/pypraaqt.py
+++ pyprag/pypraaqt.py
@@ -146,14 +146,9 @@ def build_gui(infos, frameshift, annotation=None):
         QtGui.QApplication.instance().exec_()
 
 
-
-###############################################################################
-# Main function
-###############################################################################
-def main():
+def entry_point(args, logger):
     """Main entry function
     """
-    global args
 
     # Load waves
     logger.info("Loading wav")
@@ -191,74 +186,55 @@ def main():
     infos = (wav, coef_matrix, args.wav_file)
     build_gui(infos, frameshift, annotation)
 
+def main():
+    parser = argparse.ArgumentParser(description="")
+
+    # Add options
+    parser.add_argument("-a", "--annotation_file", default=None, type=str,
+                        help="The annotation file (HTK label or TextGrid)")
+    parser.add_argument("-l", "--log_file", default=None,
+                        help="Logger file")
+    parser.add_argument("-v", "--verbosity", action="count", default=0,
+                        help="increase output verbosity")
+    parser.add_argument("-c", "--coefficient_file", default=None, type=str)
+    parser.add_argument("-d", "--dimension", default=None, type=int)
+    parser.add_argument("-f", "--frameshift", default=5, type=int)
+
+    # Add arguments
+    parser.add_argument("wav_file")
+
+    # Parsing arguments
+    args = parser.parse_args()
+
+    # create logger and formatter
+    logger = logging.getLogger()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # Verbose level => logging level
+    log_level = args.verbosity
+    if (args.verbosity >= len(LEVEL)):
+        log_level = len(LEVEL) - 1
+        logger.setLevel(log_level)
+        logging.warning("verbosity level is too high, I'm gonna assume you're taking the highest (%d)" % log_level)
+    else:
+        logger.setLevel(LEVEL[log_level])
+
+    # create console handler
+    ch = logging.StreamHandler()
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    # create file handler
+    if args.log_file is not None:
+        fh = logging.FileHandler(args.log_file)
+        logger.addHandler(fh)
+
+    # Running main function <=> run application
+    entry_point(args, logger)
+
 
 ###############################################################################
 #  Envelopping
 ###############################################################################
 if __name__ == '__main__':
-    try:
-        parser = argparse.ArgumentParser(description="")
-
-        # Add options
-        parser.add_argument("-a", "--annotation_file", default=None, type=str,
-                            help="The annotation file (HTK label or TextGrid)")
-        parser.add_argument("-l", "--log_file", default=None,
-                            help="Logger file")
-        parser.add_argument("-v", "--verbosity", action="count", default=0,
-                            help="increase output verbosity")
-        parser.add_argument("-c", "--coefficient_file", default=None, type=str)
-        parser.add_argument("-d", "--dimension", default=None, type=int)
-        parser.add_argument("-f", "--frameshift", default=5, type=int)
-
-        # Add arguments
-        parser.add_argument("wav_file")
-
-        # Parsing arguments
-        args = parser.parse_args()
-
-        # create logger and formatter
-        logger = logging.getLogger()
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-        # Verbose level => logging level
-        log_level = args.verbosity
-        if (args.verbosity >= len(LEVEL)):
-            log_level = len(LEVEL) - 1
-            logger.setLevel(log_level)
-            logging.warning("verbosity level is too high, I'm gonna assume you're taking the highest (%d)" % log_level)
-        else:
-            logger.setLevel(LEVEL[log_level])
-
-        # create console handler
-        ch = logging.StreamHandler()
-        ch.setFormatter(formatter)
-        logger.addHandler(ch)
-
-        # create file handler
-        if args.log_file is not None:
-            fh = logging.FileHandler(args.log_file)
-            logger.addHandler(fh)
-
-        # Debug time
-        start_time = time.time()
-        logger.info("start time = " + time.asctime())
-
-        # Running main function <=> run application
-        main()
-
-        # Debug time
-        logging.info("end time = " + time.asctime())
-        logging.info('TOTAL TIME IN MINUTES: %02.2f' %
-                     ((time.time() - start_time) / 60.0))
-
-        # Exit program
-        sys.exit(0)
-    except KeyboardInterrupt as e:  # Ctrl-C
-        raise e
-    except SystemExit:  # sys.exit()
-        pass
-    except Exception as e:
-        logging.error('ERROR, UNEXPECTED EXCEPTION')
-        logging.error(str(e))
-        traceback.print_exc(file=sys.stderr)
-        sys.exit(-1)
+    main()
