2c2b8965ee83577c6965202b31f2aee007b7406c
Sebastien Le Maguer <lemagues@tcd.ie>
2021-06-10 20:29:59 +0100

    [entry] update: proper player widget, some cleaning and isolate main function for better "standalone" app support

--- pyprag/pypraaqt.py
+++ pyprag/pypraaqt.py
@@ -33,9 +33,6 @@ from pyqtgraph.dockarea import *
 from pyqtgraph.Qt import QtCore, QtGui, QtWidgets
 import pyqtgraph as pg
 
-# Player
-from pyprag.audio.player import player
-
 # Signal processing helpers
 from pyprag.sig_proc.spectrum import *
 
@@ -45,6 +42,7 @@ from pyprag.annotation.textgrid import *
 
 # GUI
 from pyprag.gui.one_shot import *
+from pyprag.gui.widgets import PlayerWidget
 
 ###############################################################################
 # global constants
@@ -52,7 +50,7 @@ from pyprag.gui.one_shot import *
 LEVEL = [logging.WARNING, logging.INFO, logging.DEBUG]
 
 # Interpret image data as row-major instead of col-major
-pg.setConfigOptions(imageAxisOrder='row-major')
+pg.setConfigOptions(imageAxisOrder="row-major")
 
 ###############################################################################
 # Functions
@@ -61,36 +59,31 @@ class GUIVisu(QtGui.QMainWindow):
     def __init__(self, infos, frameshift, annotation):
         super().__init__()
 
-        self.wav = infos[0]
-        self.coef = infos[1]
-        self.filename = infos[2]
-        self.plot_area = OneShotArea(self.wav, self.coef, frameshift, annotation)
+        self._wav = infos[0]
+        self._coef = infos[1]
+        self._filename = infos[2]
+        self._plot_area = OneShotArea(self._wav, self._coef, frameshift, annotation)
 
         ##########################################
         # Setup the status bar
         ##########################################
-        self.status = QtWidgets.QStatusBar()
-        self.status.setMaximumHeight(20)
-        self.status.showMessage(self.filename)
+        self._status = QtWidgets.QStatusBar()
+        self._status.setMaximumHeight(20)
+        self._status.showMessage(self._filename)
 
         ##########################################
         # Define the left part of the window
         ##########################################
         left_layout = QtWidgets.QVBoxLayout()
-        left_layout.addWidget(self.plot_area)
-        left_layout.addWidget(self.status)
+        left_layout.addWidget(self._plot_area)
+        left_layout.addWidget(self._status)
 
         ##########################################
         # Define the right part of the window
         ##########################################
-        # Define play button
-        self.bPlay = QtWidgets.QPushButton("Play", self)
-        self.bPlay.clicked.connect(self.play)
-        self.bPlay.setDefault(False)
-        self.bPlay.setAutoDefault(False)
-
+        player_widget = PlayerWidget(self._filename, self._wav)
         right_layout = QtWidgets.QVBoxLayout()
-        right_layout.addWidget(self.bPlay)
+        right_layout.addWidget(player_widget)
 
         ##########################################
         # Finalize the main part layout
@@ -106,35 +99,37 @@ class GUIVisu(QtGui.QMainWindow):
         cent_widget.setLayout(main_layout)
         self.setCentralWidget(cent_widget)
 
-    def play(self):
-        # Play subpart
-        player.play(self.wav[0], self.wav[1])
 
 def build_gui(infos, frameshift, annotation=None):
 
     # Generate application
     app = QtGui.QApplication(["ok"])
     win = GUIVisu(infos, frameshift, annotation)
-    win.setWindowTitle('pyqtgraph example: PlotWidget')
+    win.setWindowTitle("pyqtgraph example: PlotWidget")
 
     # Add exit shortcut!
-    win.actionExit = QtGui.QAction(('E&xit'), win)
+    win.actionExit = QtGui.QAction(("E&xit"), win)
     win.actionExit.setShortcut(QtGui.QKeySequence("Ctrl+Q"))
     win.addAction(win.actionExit)
     win.actionExit.triggered.connect(win.close)
 
     # Start the application
     win.show()
-    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, "PYQT_VERSION"):
         app.exec()
 
 
 def entry_point(args, logger):
-    """Main entry function
-    """
+    """Main entry function"""
 
-    if (args.annotation_file == "") and (args.wav_file == "") and (args.coefficient_file == ""):
-        logger.error("You need to give an annotation file (-a, --annotation-file), a coefficient file (-c, --coefficient-file) and/or a wav file (-w, --wav-file)")
+    if (
+        (args.annotation_file == "")
+        and (args.wav_file == "")
+        and (args.coefficient_file == "")
+    ):
+        logger.error(
+            "You need to give an annotation file (-a, --annotation-file), a coefficient file (-c, --coefficient-file) and/or a wav file (-w, --wav-file)"
+        )
         sys.exit(-1)
 
     # Load waves
@@ -144,7 +139,7 @@ def entry_point(args, logger):
         wav = librosa.core.load(args.wav_file, sr=None)
 
     # Convert frameshift from ms to s
-    frameshift = args.frameshift/1000
+    frameshift = args.frameshift / 1000
 
     # Compute spectrum
     coef_matrix = None
@@ -152,11 +147,13 @@ def entry_point(args, logger):
         if wav is not None:
             logger.info("Compute spectrogram")
             sp_analyzer = SpectrumAnalysis(wav, frameshift=frameshift)
-            coef_matrix = sp_analyzer.spectrum
+            coef_matrix = sp_analyzer._spectrum
     else:
         logger.info("Loading coefficient file")
         if args.dimension is None:
-            raise Exception("The coefficient file is given but not the dimension of the coefficient vector")
+            raise Exception(
+                "The coefficient file is given but not the dimension of the coefficient vector"
+            )
 
         coef_matrix = np.fromfile(args.coefficient_file, dtype=np.float32)
         coef_matrix = coef_matrix.reshape((args.dimension, -1)).T
@@ -177,38 +174,55 @@ def entry_point(args, logger):
     infos = (wav, coef_matrix, args.wav_file)
     build_gui(infos, frameshift, annotation)
 
+
 def main():
     parser = argparse.ArgumentParser(description="")
 
     # Add options
-    parser.add_argument("-a", "--annotation-file", default="", type=str,
-                        help="The annotation file (HTK label or TextGrid)")
-    parser.add_argument("-l", "--log-file", default="",
-                        help="Logger file")
-    parser.add_argument("-v", "--verbosity", action="count", default=0,
-                        help="increase output verbosity")
-    parser.add_argument("-c", "--coefficient-file", default="", type=str,
-                        help="The coefficient file")
-    parser.add_argument("-d", "--dimension", default=0, type=int,
-                        help="The dimension of the coefficient vector")
-    parser.add_argument("-f", "--frameshift", default=5, type=int,
-                        help="The frameshift in milliseconds")
-    parser.add_argument("-w", "--wav_file", default="", type=str,
-                        help="The wave file")
+    parser.add_argument(
+        "-a",
+        "--annotation-file",
+        default="",
+        type=str,
+        help="The annotation file (HTK label or TextGrid)",
+    )
+    parser.add_argument("-l", "--log-file", default="", help="Logger file")
+    parser.add_argument(
+        "-v", "--verbosity", action="count", default=0, help="increase output verbosity"
+    )
+    parser.add_argument(
+        "-c", "--coefficient-file", default="", type=str, help="The coefficient file"
+    )
+    parser.add_argument(
+        "-d",
+        "--dimension",
+        default=0,
+        type=int,
+        help="The dimension of the coefficient vector",
+    )
+    parser.add_argument(
+        "-f", "--frameshift", default=5, type=int, help="The frameshift in milliseconds"
+    )
+    parser.add_argument("-w", "--wav_file", default="", type=str, help="The wave file")
 
     # Parsing arguments
     args = parser.parse_args()
 
     # create logger and formatter
     logger = logging.getLogger()
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
 
     # Verbose level => logging level
     log_level = args.verbosity
-    if (args.verbosity >= len(LEVEL)):
+    if args.verbosity >= len(LEVEL):
         log_level = len(LEVEL) - 1
         logger.setLevel(log_level)
-        logging.warning("verbosity level is too high, I'm gonna assume you're taking the highest (%d)" % log_level)
+        logging.warning(
+            "verbosity level is too high, I'm gonna assume you're taking the highest (%d)"
+            % log_level
+        )
     else:
         logger.setLevel(LEVEL[log_level])
 
@@ -229,5 +243,5 @@ def main():
 ###############################################################################
 #  Envelopping
 ###############################################################################
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
