5fa74ebd7f74f4a5b3ccc953acc47b4f849db0ba
Sebastien Le Maguer <lemagues@tcd.ie>
2022-12-31 13:09:14 +0000

    [plugins/wavelet] fix: only load if the wavelet toolkit in the pythonpath

--- pyprag/plugins/wavelet/__init__.py
+++ pyprag/plugins/wavelet/__init__.py
@@ -1,14 +1,16 @@
-from pyprag.core import plugin_entry_dict
-from .extractor import WaveletExtractor
-from .control import WaveletController
-from .visualisation import WaveletPlotWidget
+try:
+    from pyprag.core import plugin_entry_dict
+    from .extractor import WaveletExtractor
+    from .control import WaveletController
+    from .visualisation import WaveletPlotWidget
 
+    sp_extractor = WaveletExtractor()
+    sp_widget = WaveletPlotWidget(sp_extractor)
+    controller = WaveletController(sp_extractor, sp_widget)
 
-sp_extractor = WaveletExtractor()
-sp_widget = WaveletPlotWidget(sp_extractor)
-controller = WaveletController(sp_extractor, sp_widget)
+    assert controller._name not in plugin_entry_dict
+    plugin_entry_dict[controller._name] = controller
 
-assert controller._name not in plugin_entry_dict
-plugin_entry_dict[controller._name] = controller
-
-__all__ = ["WaveletExtractor", "WaveletPlotWidget", "WaveletController", "controller"]
+    __all__ = ["WaveletExtractor", "WaveletPlotWidget", "WaveletController", "controller"]
+except ModuleNotFoundError:
+    pass
